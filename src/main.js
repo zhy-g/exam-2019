1f08c1f46a592dd9697661ce478d6f6feacc92e7
zhy-g <1926963751@qq.com>
2019-12-25 15:03:01 +0800

    change

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import FastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+// 导入swiper样式表
+// import 'swiper/dist/css/swiper.css'
+// import './assets/styles/iconfont/iconfont.css'
+
+Vue.config.productionTip = false
+// 调用fastclick绑定到document.body上
+FastClick.attach(document.body)
+// 调用VueAwesomeSwiper
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/Home/Header.vue
@@ -0,0 +1,46 @@
+<template>
+   <div class="wrapper">
+       <swiper :option="swiperOption">
+           <!-- slides -->
+           <swiper-slide  v-for="item of swiperList" :key='item.id'>
+               <span >{{item.content}}</span>
+           </swiper-slide>
+           <!-- Optional controls -->
+           <!-- 利用v-for来循环遍历 swiperList 数组，添加绑定Key值来实现轮播的顺序 -->
+           <div class="swiper-pagination" slot="pagination"></div>
+       </swiper>
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader',
+    data () {
+        return {
+            swiperOption: {
+                // 添加pagination的选项并赋值.swiper-pagination来实现轮播图上圆点选项
+                pagination: '.swiper-pagination',
+                // 添加loop选项来实现轮播图的循环
+            },
+            swiperList: [
+                {
+                    id: '001',
+                    content: 'HTML'
+                },
+                {
+                    id: '002',
+                    content: 'CSS'
+                },
+                {
+                    id: '003',
+                    content: 'Vue'
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
